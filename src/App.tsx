94b89c1d6293d009d201be912e6f5faa71d19b7f
korney <madkorney@gmail.com>
2022-11-05 14:57:43 +0300

    init: create initial app structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div>hello there</div>
+    </div>
+  );
+}
+
+export default App;
