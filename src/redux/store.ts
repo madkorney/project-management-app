13153da4d817210c8338cc4b1a20991425a9226d
Kornull <wolod1987kor@gmail.com>
2022-11-13 22:30:51 +0300

    feat: add check autorization reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import { baseApiSlice } from 'services';
 import authSliceReducer from './authSlice';
 import showPasswordReducer from './showUserPasswordSlice';
+import authorizedReducer from './authorizedSlice';
 
 const reducer = {
   auth: authSliceReducer,
   password: showPasswordReducer,
+  authorized: authorizedReducer,
   [baseApiSlice.reducerPath]: baseApiSlice.reducer,
 };
 export default configureStore({
