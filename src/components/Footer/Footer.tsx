bdb69453506c06893945c6ef251f6035c7efa5dc
Kornull <wolod1987kor@gmail.com>
2022-11-13 22:39:46 +0300

    fix: footer module

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <div>git1</div>
+        <div>git2</div>
+        <div>git3</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
