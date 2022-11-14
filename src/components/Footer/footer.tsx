c1d1dd073c16489ee4ff996e1bc90f95b0597f73
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-11-15 00:48:24 +0300

    refactor: refactor footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,50 +1,27 @@
 import { Link } from 'react-router-dom';
-import AnchorLink from '@mui/material/Link';
+import GithubLink from './GithubLink';
 import './footer.scss';
-
-// import './style.scss';
 
 const Footer = () => (
   <footer className="footer">
     <div className="container footer-container">
       <div className="github">
-        <AnchorLink
-          className="link github__madkorney"
-          href="https://github.com/madkorney"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="madkorney GitHub"
-        ></AnchorLink>
-        <AnchorLink
-          className="link github__kornull"
-          href="https://github.com/Kornull"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Kornull GitHub"
-        ></AnchorLink>
-        <AnchorLink
-          className="link github__sylarbrest"
-          href="https://github.com/sylarBrest"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="sylarBrest GitHub"
-        ></AnchorLink>
+        <GithubLink name="madkorney" />
+        <GithubLink name="Kornull" />
+        <GithubLink name="sylarBrest" />
       </div>
-      <div className="copyright">
-        <Link to="/about">
-          <span>©</span>
-          <span> 2022 </span>
-          <span>Team 7</span>
-        </Link>
-      </div>
-      <div className="rss-logo">
-        <a
-          href="https://rs.school/react"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Rolling Scope School"
-        ></a>
-      </div>
+      <Link to="/about" className="copyright">
+        <span>©</span>
+        <span> 2022 </span>
+        <span>Team 7</span>
+      </Link>
+      <a
+        className="rss-logo"
+        href="https://rs.school/react"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Rolling Scope School"
+      ></a>
     </div>
   </footer>
 );
