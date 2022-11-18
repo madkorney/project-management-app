3313ca9e74e627a7af43d21401c4e921c0297cc8
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-11-18 15:55:09 +0300

    feat: add boards page basic layout

--- src/pages/BoardsPage/boardsPage.tsx
+++ src/pages/BoardsPage/boardsPage.tsx
@@ -1,7 +1,29 @@
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import { useAppSelector } from 'redux/hooks';
+import { useGetBoardsSetByUserIdQuery } from 'services';
+
+import './boardsPage.scss';
+
 const BoardsPage = () => {
+  const userId = useAppSelector((store) => store.auth.user.id) as string;
+  const { data } = useGetBoardsSetByUserIdQuery(userId);
+
   return (
-    <div>
-      <p>Here we can see our boards</p>
+    <div className="boards-container">
+      {data &&
+        data.map((board) => (
+          <Card sx={{ width: 240 }} key={board._id}>
+            <CardContent>
+              <Typography variant="h5">{board.title}</Typography>
+              <Typography>{board.description}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" variant="contained">
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
     </div>
   );
 };
