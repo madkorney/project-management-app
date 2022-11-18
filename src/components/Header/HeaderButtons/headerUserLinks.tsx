ca362d6e6e56f5b65b701ac6eae360f028ac86b9
Kornull <wolod1987kor@gmail.com>
2022-11-18 20:08:37 +0300

    feat: add user links component

--- src/components/Header/HeaderButtons/headerUserLinks.tsx
+++ src/components/Header/HeaderButtons/headerUserLinks.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-const HeaderUserLink = () => {
+export const HeaderUserLinks = () => {
   return (
     <>
       <li>
--- src/components/Header/HeaderButtons/index.ts
+++ src/components/Header/HeaderButtons/index.ts
@@ -1,1 +1,2 @@
 export * from './headerUserButtons';
+export * from './headerUserLinks';
