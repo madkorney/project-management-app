7363aae0a9eb94b5e520c8c99ada0ee63c22ef0f
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-11-18 16:48:44 +0300

    feat: add delete button on card

--- src/pages/BoardsPage/boardsPage.tsx
+++ src/pages/BoardsPage/boardsPage.tsx
@@ -1,27 +1,37 @@
 import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 import { useAppSelector } from 'redux/hooks';
 import { useGetBoardsSetByUserIdQuery } from 'services';
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 
 import './boardsPage.scss';
 
 const BoardsPage = () => {
   const userId = useAppSelector((store) => store.auth.user.id) as string;
   const { data } = useGetBoardsSetByUserIdQuery(userId);
-  console.log(data);
+
+  const handleDeleteClick = (event: React.MouseEvent) => {
+    console.log(event.target);
+  };
 
   return (
     <div className="boards-container">
       {data &&
         data.map((board) => (
-          <Card sx={{ width: 240 }} key={board._id}>
+          <Card sx={{ width: 240, backgroundColor: '#b4b4b4' }} key={board._id}>
             <CardContent>
               <Typography variant="h5">{board.title}</Typography>
               <Typography>{board.description}</Typography>
             </CardContent>
             <CardActions>
               <Button size="small" variant="contained">
-                Learn More
+                Open
               </Button>
+              <DeleteForeverOutlinedIcon
+                id={board._id}
+                onClick={handleDeleteClick}
+                fontSize="large"
+                sx={{ marginLeft: 'auto' }}
+              />
             </CardActions>
           </Card>
         ))}
