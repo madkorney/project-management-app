b0076a3c92b917710fcf97835cd6a33d3118f7ec
Kornull <wolod1987kor@gmail.com>
2022-11-19 11:19:56 +0300

    feat: add userSettingsSlice

--- src/redux/modalUserSlice.ts
+++ src/redux/modalUserSlice.ts
@@ -1,34 +1,66 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+export type UpdateUser = {
+  name: string;
+  login: string;
+  password: string;
+};
+
 type ModalUser = {
-  open: boolean;
-  textUpdate: string;
-  textDelete: string;
-  modalType: string;
+  isOpen: boolean;
+  isOpenUserPage: boolean;
+  isModalType: boolean;
+  isMessageUser: string;
+  updateUserData: UpdateUser;
 };
 
 const initialState: ModalUser = {
-  open: false,
-  textUpdate: 'Are you sure you want to update your account?',
-  textDelete: 'Are you sure you want to delete your account?',
-  modalType: '',
+  isOpen: false,
+  isOpenUserPage: false,
+  isModalType: false,
+  isMessageUser: '',
+  updateUserData: {
+    name: '',
+    login: '',
+    password: '',
+  },
 };
 
 const userUpdateData = createSlice({
   name: 'userAccount',
   initialState,
   reducers: {
-    setOpenUpdateModal: (state, action: PayloadAction<string>) => {
-      state.open = true;
-      state.modalType = action.payload;
+    setOpenUpdateModal: (state, action: PayloadAction<boolean>) => {
+      state.isOpen = true;
+      state.isModalType = action.payload;
     },
     setCloseModal: (state, action: PayloadAction<boolean>) => {
-      state.open = action.payload;
-      state.modalType = '';
+      state.isOpen = action.payload;
+      state.isOpenUserPage = false;
+      // state.updateUserData = initialState.updateUserData;
+      // state.isMessageUser = '';
+    },
+    setOpenUserPage: (state, action: PayloadAction<boolean>) => {
+      state.isOpenUserPage = action.payload;
+    },
+    setUpdateUserData: (state, action: PayloadAction<UpdateUser>) => {
+      state.updateUserData = action.payload;
+      state.isOpen = true;
+      state.isOpenUserPage = false;
+      state.isModalType = false;
+    },
+    setMessageUser: (state, action: PayloadAction<string>) => {
+      state.isMessageUser = action.payload;
     },
   },
 });
 
-export const { setOpenUpdateModal, setCloseModal } = userUpdateData.actions;
+export const {
+  setOpenUpdateModal,
+  setCloseModal,
+  setOpenUserPage,
+  setUpdateUserData,
+  setMessageUser,
+} = userUpdateData.actions;
 
 export default userUpdateData.reducer;
