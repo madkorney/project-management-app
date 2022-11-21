c9f51ed635f769e5acc9133cfa4c1641a85a637d
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-11-21 16:19:57 +0300

    feat: add assignment of users to boards

--- src/components/Forms/addBoard.tsx
+++ src/components/Forms/addBoard.tsx
@@ -2,10 +2,10 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { useAppSelector } from 'redux/hooks';
 
-import { Button, TextField } from '@mui/material';
+import { Autocomplete, Button, TextField } from '@mui/material';
 import { Toast } from 'components';
 
-import { useCreateBoardMutation } from 'services';
+import { useCreateBoardMutation, useGetUsersQuery } from 'services';
 import { BoardParamsType, ErrorResponse } from 'types';
 
 import './addBoard.scss';
@@ -18,14 +18,16 @@ const AddBoard = ({ onClose }: AddBoardType) => {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm<BoardParamsType>();
 
   const [addBoard, { error }] = useCreateBoardMutation();
   const userId = useAppSelector((state) => state.auth.user.id) as string;
+  const { data: users } = useGetUsersQuery();
 
-  const onSubmit: SubmitHandler<Pick<BoardParamsType, 'title' | 'description'>> = async (data) => {
-    await addBoard({ ...data, owner: userId, users: [] }).unwrap();
+  const onSubmit: SubmitHandler<Omit<BoardParamsType, 'owner'>> = async (data) => {
+    await addBoard({ ...data, owner: userId }).unwrap();
     onClose?.();
   };
 
@@ -57,9 +59,35 @@ const AddBoard = ({ onClose }: AddBoardType) => {
             value: true,
             message: 'Description cannot be empty',
           },
+          minLength: {
+            value: 20,
+            message: 'Please write more detailed description',
+          },
         })}
       />
       {errors.description && <span>{errors.description.message}</span>}
+      {users && (
+        <Autocomplete
+          multiple
+          id="users"
+          options={users.filter((user) => user._id !== userId)}
+          getOptionLabel={(option) => option.name}
+          filterSelectedOptions
+          noOptionsText="No users found"
+          {...register('users', {
+            required: true,
+          })}
+          onChange={(_, value) =>
+            setValue(
+              'users',
+              value.map((key) => key._id)
+            )
+          }
+          renderInput={(params) => (
+            <TextField {...params} variant="standard" label="Assigned users" placeholder="Users" />
+          )}
+        />
+      )}
       <Button variant="contained" type="submit">
         Add
       </Button>
