6babc13b3057625e6b1d125f2880c17338fb3ee0
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-12-03 18:05:20 +0300

    refactor: fix footer styles and add some text on main page

--- src/pages/MainPage/mainPage.tsx
+++ src/pages/MainPage/mainPage.tsx
@@ -1,7 +1,14 @@
 const MainPage = () => {
   return (
     <div>
-      <p>main page</p>
+      <p style={{ fontSize: '40px' }}>
+        With the Project Tracker you can effectively manage your projects, share with the teammates,
+        create columns and name them according to your requirements, you can change the order of the
+        columns if necessary. In each column, you can create tasks, assign performers, move them
+        both within the column and between columns.
+      </p>
+      <p>You can create an unlimited number of projects, columns and tasks.</p>
+      <p>Use our app and leave feedback!</p>
     </div>
   );
 };
