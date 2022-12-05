7edcb62b258a5c67082094134426506e58eb97f9
Kornull <wolod1987kor@gmail.com>
2022-12-05 13:42:49 +0300

    refactor: style userPage

--- src/pages/UserPage/userPage.tsx
+++ src/pages/UserPage/userPage.tsx
@@ -33,7 +33,7 @@ const UserPage = () => {
   const [updateUser, { error }] = useUpdateUserByIdMutation();
   const { id } = useAppSelector((state) => state.auth.user!);
   const [isMessageUser, setMessageUser] = useState('');
-  const [userNmae, setUserName] = useState('');
+  const [userName, setUserName] = useState('');
   const userData = useGetUserByIdQuery(id);
 
   const handleDeleteUser = async () => {
@@ -42,6 +42,7 @@ const UserPage = () => {
     dispatch(logOut());
     navigate('/');
   };
+  console.log('run');
 
   const onSubmit = async (dataUser: UserSignUpType) => {
     id &&
@@ -79,8 +80,8 @@ const UserPage = () => {
       {error && <Toast message={(error as ErrorResponse).data.message} />}
       {!error && isMessageUser && <Toast message={isMessageUser} />}
       <div className={styles.userDescription}>
-        <h2>
-          {t('userHi')}, {userNmae && userNmae.slice(0, 1).toUpperCase() + userNmae.slice(1)}
+        <h2 className={styles.userTitle}>
+          {t('userHi')} {userName && userName.slice(0, 1).toUpperCase() + userName.slice(1)}!
         </h2>
         <p>{t('userPage')}</p>
       </div>
